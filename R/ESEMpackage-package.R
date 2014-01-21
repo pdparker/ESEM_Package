97bc68fc2a14c2adcc16de1aba2903a276d3205b
Philip Parker <philipparker@Philip-Parkers-MacBook-Pro.local>
2014-01-22 09:36:47 +1100

    Added simulation data

---
+++ R/ESEMpackage-package.R
@@ -0,0 +1,23 @@
+#' ESEM script writer.
+#'
+#' \tabular{ll}{
+#' Package: \tab ESEMpackage\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2014-01-22\cr
+#' Depends: \tab R (>= 2.10.0)\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Writes multi group ESEM invariance scripts for MPLUS for 13 model
+#' taxonomy of invariance.
+#'
+#' @aliases ESEMpackage-package ESEMpackage
+#' @name ESEMpackage-package
+#' @docType package
+#' @title The ESEMpackage Package
+#' @author Philip Parker \email{philip.parker@@uws.edu.au}
+#' @keywords package
+NULL
